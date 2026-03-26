385fe7dec03472ecb15c771c02869564a4126230
smpate111 <sahilp1424@gmail.com>
2026-03-26 10:15:52 -0700

    updated project and organized files

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,145 @@
+# === LIBRARIES ===
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+
+from launch_ros.actions import Node
+# =================
+
+def generate_launch_description():
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='mobile_robot' #<--- CHANGE ME
+
+    # this is a switch for using ros2_control or diff drive
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+    # create robot state publisher
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+                    ]
+                ),
+                launch_arguments={
+                    'use_sim_time': 'true',
+                    'use_ros2_control': use_ros2_control
+                }.items()
+    )
+    
+    # create world
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        #default_value="empty.sdf",
+        default_value=os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world'),
+        description='World to load'
+        )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config/yaml', 'gazebo_params.yaml')
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+            ]
+        ),
+        launch_arguments={
+            'gz_args': [
+                '-r -v4 ', world,
+            ],
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'mobile_robot',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
+
+    # Launch the ROS-Gazebo bridge for normal topics
+    bridge_params = os.path.join(get_package_share_directory(package_name), 'config/yaml', 'gz_bridge.yaml')
+
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    # camera
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=[
+            "/camera/image_raw",
+            "/depth_camera/image_raw"
+        ]
+    )
+
+    # ros2 control
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_cont",
+            '--controller-ros-args',
+            '-r /diff_cont/cmd_vel:=/cmd_vel'
+        ],
+        parameters=[
+            {
+                'use_sim_time': True
+            }
+        ],
+        condition=IfCondition(use_ros2_control)
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        parameters=[
+            {
+                'use_sim_time': True
+            }
+        ],
+        condition=IfCondition(use_ros2_control)
+    )
+
+    # Launch them all!
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                'use_ros2_control',
+                default_value='true',
+                description='Use ros2_control if true'
+            ),
+            rsp,
+            world_arg,
+            gazebo,
+            spawn_entity,
+            ros_gz_bridge,
+            ros_gz_image_bridge,
+            diff_drive_spawner,
+            joint_broad_spawner
+        ]
+    )
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 import xacro
 
@@ -13,15 +14,29 @@ import xacro
 def generate_launch_description():
 
     # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # Check if we are using ros2_control
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('mobile_robot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file).toxml()
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+
+    #robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(
+        [
+            'xacro ', xacro_file,
+            ' use_ros2_control:=', use_ros2_control
+        ]
+    )
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params = {
+        'robot_description': ParameterValue(robot_description_config, value_type=str),
+        'use_sim_time': use_sim_time
+    }
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -31,11 +46,20 @@ def generate_launch_description():
 
 
     # Launch!
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='true',
+                description='Use sim time if true'
+            ),
 
-        node_robot_state_publisher
-    ])
+            DeclareLaunchArgument(
+                'use_ros2_control',
+                default_value='true',
+                description='Use ros2_control if true'
+            ),
+
+            node_robot_state_publisher
+        ]
+    )
